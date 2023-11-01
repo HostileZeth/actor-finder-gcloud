72c2456890dbb026a68d750552674a2b3011dadf
sergey.ataev <sergey.ataev@waveaccess.global>
2023-11-01 15:01:39 +0400

    added user-log-function deploy bat

---
+++ user-log-function/deploy-function.bat
@@ -0,0 +1,2 @@
+gcloud functions deploy message-log-function --region=europe-west1 --gen2 --runtime=java17 --entry-point=com.mitsudoku.functions.HelloWorld --trigger-topic=actor-stats-cloud --env-vars-file .env.yaml
+pause
